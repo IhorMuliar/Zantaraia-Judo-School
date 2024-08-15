a74a669f2ccd924adfc478b0d60e9ecd6f2dd6f7
Kiril-Muliar <142143082+Kiril-Muliar@users.noreply.github.com>
2024-08-15 12:56:36 +0300

    update schedule table (#7)
    * update schedule table
    
    * minor fix

--- app/schedule/_components/schedule-table.jsx
+++ app/schedule/_components/schedule-table.jsx
@@ -1,7 +1,7 @@
 const ScheduleTable = () => {
   return (
     <>
-      <table className="table-responsive-md ck-table">
+      <table className="table-responsive-md ck-table mb-5">
         <thead>
           <tr>
             <th></th>
@@ -10,193 +10,80 @@ const ScheduleTable = () => {
             <th>Середа</th>
             <th>Четвер</th>
             <th>П'ятниця</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="row_1">
+            <td className="event-time"> 08:00 - 09:30 </td>
+            <td className="event title" rowSpan="2">Старша група</td>
+            <td className="event title" rowSpan="2">Старша група</td>
+            <td className="event title" rowSpan="2">Старша група</td>
+            <td className="event title" rowSpan="2">Старша група</td>
+            <td className="event title" rowSpan="2">Старша група</td>
+          </tr>
+          <tr className="row_2">
+            <td className="event-time"> 09:30 - 11:00 </td>
+          </tr>
+          <tr className="row_3">
+            <td className="event-time"> 14:00 - 15:00 </td>
+            <td className="event title">Група 6-8 років</td>
+            <td></td>
+            <td className="event title">Група 6-8 років</td>
+            <td></td>
+            <td className="event title">Група 6-8 років</td>
+          </tr>
+          <tr className="row_4">
+            <td className="event-time"> 15:00 - 16:30 </td>
+            <td className="event title">Група 8-10 років</td>
+            <td className="event title">Група 8-10 років</td>
+            <td className="event title">Група 8-10 років</td>
+            <td className="event title">Група 8-10 років</td>
+            <td className="event title">Група 8-10 років</td> 
+          </tr>
+          <tr className="row_5">
+            <td className="event-time"> 16:30 - 18:00 </td>
+            <td className="event title">Група 10-14 років</td>
+            <td className="event title">Група 10-14 років</td>
+            <td className="event title">Група 10-14 років</td>
+            <td className="event title">Група 10-14 років</td>
+            <td className="event title">Група 10-14 років</td>
+          </tr>
+          <tr className="row_6">
+            <td className="event-time"> 18:00 - 19:30</td>
+            <td className="event title" rowSpan="2">Старша група</td>
+            <td className="event title">Старша група</td>
+            <td className="event title" rowSpan="2">Старша група</td>
+            <td className="event title">Старша група</td>
+            <td className="event title" rowSpan="2">Старша група</td>
+          </tr>
+          <tr className="row_7">
+            <td className="event-time"> 19:30 - 20:15 </td>
+            <td className="event title">Група 5-6 років</td>
+            <td className="event title">Група 5-6 років</td>
+          </tr>
+        </tbody>
+      </table>
+      <table className="table-responsive-md ck-table">
+        <thead>
+          <tr>
+            <th></th>
             <th>Субота</th>
             <th>Неділя</th>
           </tr>
         </thead>
         <tbody>
           <tr className="row_1">
-            <td className="event-time"> 06:00 - 07:00 </td>
-            <td className="event title" rowSpan="2">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
-            <td className="event title" rowSpan="2">
-              Crossfit Class
-              <span className="subtitle">Madison Fren</span>
-              <div className="event-tag">Hit Training</div>
-            </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td className="event title" rowSpan="4">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
+            <td className="event-time"> 08:00 - 09:30 </td>
+            <td className="event title" rowSpan="1">Старша група</td>
+            <td className="event title" rowSpan="3">Вихідні</td>
           </tr>
           <tr className="row_2">
-            <td className="event-time"> 07:00 - 08:00 </td>
-            <td className="event title" rowSpan="3">
-              Power Lifting
-              <span className="subtitle">Joe Bell</span>
-              <div className="event-tag">Strenght</div>
-            </td>
-            <td></td>
-            <td className="event title" rowSpan="3">
-              Power Lifting
-              <span className="subtitle">Joe Bell</span>
-              <div className="event-tag">Strenght</div>
-            </td>
-            <td className="event title">
-              Cardio Burn
-              <span className="subtitle">Anna Simms Jr.</span>
-              <div className="event-tag">Aerobic</div>
-            </td>
+            <td className="event-time"> 10:00 - 11:30 </td>
+            <td className="event title">Старша група</td>
           </tr>
           <tr className="row_3">
-            <td className="event-time"> 08:00 - 09:00 </td>
-            <td></td>
-            <td className="event title" rowSpan="4">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr className="row_4">
-            <td className="event-time"> 09:00 - 10:00 </td>
-            <td></td>
-            <td className="event title" rowSpan="6">
-              Crossfit Class
-              <span className="subtitle">Madison Fren</span>
-              <div className="event-tag">Hit Training</div>
-            </td>
-            <td></td>
-          </tr>
-          <tr className="row_5">
-            <td className="event-time"> 10:00 - 11:00 </td>
-            <td className="event title" rowSpan="2">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
-            <td></td>
-            <td className="event title">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
-            <td className="event title" rowSpan="2">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
-            <td></td>
-          </tr>
-          <tr className="row_6">
-            <td className="event-time"> 11:00 - 12:00 </td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr className="row_7">
-            <td className="event-time"> 12:00 - 13:00 </td>
-            <td className="event title">
-              Power Lifting
-              <span className="subtitle">Joe Bell</span>
-              <div className="event-tag">Strength</div>
-            </td>
-            <td></td>
-            <td className="event title">
-              Cardio Burn
-              <span className="subtitle">Anna Simms Jr.</span>
-              <div className="event-tag">Aerobic</div>
-            </td>
-            <td className="event title">
-              Crossfit Class
-              <span className="subtitle">Madison Fren</span>
-              <div className="event-tag">Hit Training</div>
-            </td>
-            <td className="event title">
-              Crossfit Class
-              <span className="subtitle">Madison Fren</span>
-              <div className="event-tag">Hit Training</div>
-            </td>
-            <td></td>
-          </tr>
-          <tr className="row_8">
-            <td className="event-time"> 13:00 - 14:00 </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr className="row_9">
-            <td className="event-time"> 14:00 - 15:00 </td>
-            <td></td>
-            <td className="event title" rowSpan="2">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
-            <td className="event title" rowSpan="2">
-              Open Gym
-              <div className="event-tag">Gym</div>
-            </td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr className="row_10">
-            <td className="event-time"> 15:00 - 16:00 </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr className="row_11">
-            <td className="event-time"> 18:00 - 19:00 </td>
-            <td className="event title" rowSpan="2">
-              Crossfit Class
-              <span className="subtitle">Madison Fren</span>
-              <div className="event-tag">Hit Training</div>
-            </td>
-            <td className="event title" rowSpan="2">
-              Power Lifting
-              <span className="subtitle">Joe Bell</span>
-              <div className="event-tag">Strength</div>
-            </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr className="row_12">
-            <td className="event-time"> 19:00 - 20:00 </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-          </tr>
-          <tr className="row_13">
-            <td className="event-time"> 20:00 - 21:00 </td>
-            <td className="event title">
-              Cardio Burn
-              <span className="subtitle">Anna Simms Jr.</span>
-              <div className="event-tag">Aerobic</div>
-            </td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td className="event title">
-              Cardio Burn
-              <span className="subtitle">Anna Simms Jr.</span>
-              <div className="event-tag">Aerobic</div>
-            </td>
-            <td></td>
-            <td></td>
+            <td className="event-time"> 11:30 - 13:00 </td>
+            <td>Групи 8-10 років та 10-14 років</td>
           </tr>
         </tbody>
       </table>
