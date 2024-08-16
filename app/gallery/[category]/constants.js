503a38b1c7eb5e7e0ee6698b8ce598e821168a33
KirilMuliar <kirilmuliar@gmail.com>
2024-08-16 14:53:26 +0300

    update gallery

---
+++ app/gallery/[category]/constants.js
@@ -0,0 +1,11 @@
+export const allImages = [
+    { category: "competions", image: "/images/portfolio/photo5.jpg" },
+    { category: "competions", image: "/images/portfolio/photo32.jpg" },
+    { category: "competions", image: "/images/portfolio/photo21.jpg" },
+    { category: "travels", image: "/images/portfolio/photo3.jpg" },
+    { category: "travels", image: "/images/portfolio/photo22.jpg" },
+    { category: "travels", image: "/images/portfolio/photo34.jpg" },
+    { category: "sport-meetings", image: "/images/portfolio/photo4.jpg" },
+    { category: "sport-meetings", image: "/images/portfolio/photo24.jpg" },
+    { category: "sport-meetings", image: "/images/portfolio/photo9.jpg" },
+  ];
--- app/gallery/[category]/page.jsx
+++ app/gallery/[category]/page.jsx
@@ -1,15 +1,74 @@
+"use client";
+
+import { notFound } from "next/navigation";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
 import Breadcrumbs from "@/components/shared/breadcrumbs";
+import { allImages } from "./constants";
+
+const categoryTranslations = {
+  competions: "Змагання",
+  travels: "Подорожі",
+  "sport-meetings": "Спортивні зустрічі",
+};
+
+
 
 const GalleryDetails = ({ params }) => {
+  const { category } = params;
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    if (!categoryTranslations[category]) {
+      notFound();
+
+      return;
+    }
+
+    const filteredImages = allImages.filter((image) => image.category === category);
+    setImages(filteredImages);
+  }, [category]);
+
+  const translatedCategory = categoryTranslations[category];
+  
+  if (!translatedCategory) {
+    notFound();
+
+    return null;
+  }
+
   return (
     <>
       <Breadcrumbs
         parentTitle="Галерея"
         parentUrl="/gallery"
-        activePage={params.category}
+        activePage={translatedCategory}
       />
+      <section className="content-inner">
+        <div className="container">
+          <div className="row">
+            {images.map((item, index) => (
+              <div className="col-lg-4 col-sm-6 m-b40" key={index}>
+                <div className="dz-box style-2">
+                  <div className="dz-media">
+                    <Image
+                      src={item.image}
+                      width={340}
+                      height={250}
+                      quality={100}
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
 export default GalleryDetails;
+
